5404b8213a755ef1f35815d627d57f50c3f51328
jalallelrumii-png <jalallelrumii@gmail.com>
2026-02-17 09:37:54 +0700

    Update service worker to cache version v4

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('v2').then((c) => c.addAll(['./', './index.html', './manifest.json']))); // Cache version updated
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('v4').then(c => c.addAll(['./', './index.html', './manifest.json'])));
 });
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
