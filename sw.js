11c121211661bc11682b70af9741b35090fb0709
jalallelrumii-png <jalallelrumii@gmail.com>
2026-02-17 09:46:45 +0700

    Update service worker cache version to v8

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open('v4').then(c => c.addAll(['./', './index.html', './manifest.json'])));
+  e.waitUntil(caches.open('v8').then(c => c.addAll(['./', './index.html', './manifest.json'])));
 });
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
